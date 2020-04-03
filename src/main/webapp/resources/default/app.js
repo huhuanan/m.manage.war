73e6ad9ab65b9e581fbedf99aa7d79e99605f611
MOYUAN <douzhi9124@qq.com>
2020-04-03 21:48:19 +0800

    1

--- src/main/webapp/resources/default/app.js
+++ src/main/webapp/resources/default/app.js
@@ -19,6 +19,13 @@ var wxUtil={
 	}
 };
 $(function(){
+	var ua = window.navigator.userAgent.toLowerCase();
+	//console.log(ua);//mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
+	if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+		wxUtil.isWechat=true;
+	} else {
+		wxUtil.isWechat=false;
+	}
 	if(wx){
 		wx.ready(function(){
 			wxUtil.isWechat=true;
